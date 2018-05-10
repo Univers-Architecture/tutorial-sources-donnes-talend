8320864454fc356dd150f74230b2674bd1f3fd5d
unknown <zaydan.malika@gmail.com>
2018-05-10 11:01:01 +0100

    Sources

---
+++ Mapping_DB/Mapping_DB_run.bat
@@ -0,0 +1,3 @@
+%~d0
+ cd %~dp0
+ java -Xms256M -Xmx1024M -cp ../lib/systemRoutines.jar;../lib/userRoutines.jar;.;mapping_db_0_1.jar;../lib/activation.jar;../lib/advancedPersistentLookupLib-1.0.jar;../lib/commons-collections-3.2.jar;../lib/dom4j-1.6.1.jar;../lib/jboss-serialization.jar;../lib/jxl.jar;../lib/log4j-1.2.15.jar;../lib/mail-1.4.jar;../lib/mysql-connector-java-5.1.30-bin.jar;../lib/ojdbc6.jar;../lib/talend-oracle-timestamptz.jar;../lib/talendcsv.jar;../lib/talend_file_enhanced_20070724.jar;../lib/tns.jar;../lib/trove.jar; projectname.mapping_db_0_1.Mapping_DB --context=Default %* 
---
+++ Mapping_DB/Mapping_DB_run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd `dirname $0`
+ ROOT_PATH=`pwd`
+ java -Xms256M -Xmx1024M -cp $ROOT_PATH:$ROOT_PATH/../lib/systemRoutines.jar:$ROOT_PATH/../lib/userRoutines.jar::.:$ROOT_PATH/mapping_db_0_1.jar:$ROOT_PATH/../lib/activation.jar:$ROOT_PATH/../lib/advancedPersistentLookupLib-1.0.jar:$ROOT_PATH/../lib/commons-collections-3.2.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/jboss-serialization.jar:$ROOT_PATH/../lib/jxl.jar:$ROOT_PATH/../lib/log4j-1.2.15.jar:$ROOT_PATH/../lib/mail-1.4.jar:$ROOT_PATH/../lib/mysql-connector-java-5.1.30-bin.jar:$ROOT_PATH/../lib/ojdbc6.jar:$ROOT_PATH/../lib/talend-oracle-timestamptz.jar:$ROOT_PATH/../lib/talendcsv.jar:$ROOT_PATH/../lib/talend_file_enhanced_20070724.jar:$ROOT_PATH/../lib/tns.jar:$ROOT_PATH/../lib/trove.jar: projectname.mapping_db_0_1.Mapping_DB --context=Default "$@" 
